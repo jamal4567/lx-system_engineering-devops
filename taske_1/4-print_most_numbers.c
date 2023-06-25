8bd4ec1d14b619ad1e219bba679df7a78d00edf9
jamalimmiss <jamalimmiss97@gmail.com>
2023-06-25 18:16:45 +0000

    print except 4 and 2

---
+++ taske_1/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_most_numbers - print all except 2 and 4
+ *
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		if (n == 2 || n == 4)
+			continue;
+		else
+			_putchar(n + '0');
+	}
+	_putchar('\n');
+}
